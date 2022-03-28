426c8de72ca9fb77493d0725f48b918e1efc0a59
sugahara <sugaya0122@gmail.com>
2022-03-28 18:53:51 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<table border="1" style="border-collapse:collapse;">
+  <?php
+  for ($i = 1; $i < 10; $i++) {
+    echo '<tr>';
+    for ($j = 1; $j < 10; $j++) {
+      echo '<td>' . $i * $j . '</td>';
+    }
+    echo '</tr>';
+  }
+  ?>
+</table>
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES);
+
+print $name . "さんご登録ありがとうございます";
+
+?>
